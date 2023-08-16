7b35dfd08a570f6460c71ff068dd87f856f1851e
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-08-16 12:56:50 +0300

    fix substitutions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import './style.css';
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select'
+import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-
+import './style.css';
 
 const ref = {
     selector: document.querySelector('.breed-select'),
@@ -11,6 +10,7 @@ const ref = {
     loader: document.querySelector('.loader'),
     error: document.querySelector('.error'),
 };
+
 const { selector, divCatInfo, loader, error } = ref;
 
 loader.classList.replace('loader', 'is-hidden');
@@ -18,37 +18,55 @@ error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
 
 let arrBreedsId = [];
-fetchBreeds()
-.then(data => {
-    data.forEach(element => {
+
+axios.get('https://api.thecatapi.com/v1/breeds', {
+    headers: {
+        'x-api-key': 'live_lGMUTAe9W1QeOKbzlMy7xt6Nvb63P60YDLYN9UU9Uwn7PSIZFS77YTOGYADgU2FV'
+    }
+})
+.then(response => {
+    response.data.forEach(element => {
         arrBreedsId.push({text: element.name, value: element.id});
     });
     new SlimSelect({
         select: selector,
         data: arrBreedsId
     });
-    })
+})
 .catch(onFetchError);
 
 selector.addEventListener('change', onSelectBreed);
 
+function renderCatInfo(url, breed) {
+    divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breed.name}" width="400"/></div><div class="box"><h1>${breed.name}</h1><p>${breed.description}</p><p><b>Temperament:</b> ${breed.temperament}</p></div>`
+    divCatInfo.classList.remove('is-hidden');
+}
+
 function onSelectBreed(event) {
+    if (divCatInfo.classList.contains('is-hidden')) {
+        divCatInfo.classList.remove('is-hidden');
+        return;
+    }
+
     loader.classList.replace('is-hidden', 'loader');
     selector.classList.add('is-hidden');
     divCatInfo.classList.add('is-hidden');
 
     const breedId = event.currentTarget.value;
-    fetchCatByBreed(breedId)
-    .then(data => {
+    axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
+        headers: {
+            'x-api-key': 'live_lGMUTAe9W1QeOKbzlMy7xt6Nvb63P60YDLYN9UU9Uwn7PSIZFS77YTOGYADgU2FV'
+        }
+    })
+    .then(response => {
         loader.classList.replace('loader', 'is-hidden');
         selector.classList.remove('is-hidden');
-        const { url, breeds } = data[0];
-        
-        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
-        divCatInfo.classList.remove('is-hidden');
+        const { url, breeds } = response.data[0];
+
+        renderCatInfo(url, breeds[0]);
     })
     .catch(onFetchError);
-};
+}
 
 function onFetchError(error) {
     selector.classList.remove('is-hidden');
@@ -60,4 +78,4 @@ function onFetchError(error) {
         width: '400px',
         fontSize: '24px'
     });
-};
+}
